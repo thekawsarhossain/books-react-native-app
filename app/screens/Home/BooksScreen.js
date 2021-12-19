0e306a8c7c21d6a00818b609bc07863ef1eab08e
thekawsarhossain <thekawsarhossain@gmail.com>
2021-12-19 23:23:08 +0600

    search implement start

--- app/screens/Home/BooksScreen.js
+++ app/screens/Home/BooksScreen.js
@@ -1,4 +1,4 @@
-import { ActivityIndicator } from '@react-native-material/core';
+import { ActivityIndicator, TextInput } from '@react-native-material/core';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native';
@@ -8,12 +8,15 @@ import BookScreen from './BookScreen';
 function BooksScreen() {
 
     const [books, setBooks] = useState([]);
+    // const [displayBook, setDisplayBooks] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState('');
 
     const getBooks = async () => {
         try {
             const response = await fetch('https://mighty-ocean-62001.herokuapp.com/books');
             const json = await response.json();
+            // setDisplayBooks(json)
             setBooks(json);
         } catch (error) {
             console.error(error);
@@ -29,11 +32,17 @@ function BooksScreen() {
         return <View style={styles.loading}><ActivityIndicator size="large" color={colors.primary} /></View>
     }
 
+    // const searchedData = books.filter(book => book.title.toLowerCase().includes(search.toLowerCase()));
+    // setDisplayBooks(searchedData);
+
     return (
         <ScrollView>
+            {/* <View style={{ marginTop: 10 }}>
+                <TextInput placeholder='search here' onChangeText={search => setSearch(search)} />
+            </View> */}
             <View style={styles.parent}>
                 {
-                    books?.map((book) => <BookScreen
+                    books.map((book) => <BookScreen
                         key={book._id}
                         books={book}
                     />)
